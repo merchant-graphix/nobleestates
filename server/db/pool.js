f9aeed83e48b043382294846ed50ee6b46b54003
Noel Mlozi <noelmlozi265@gmail.com>
2026-07-14 15:53:54 +0200

    Configure PostgreSQL for Render

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,18 +1,22 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.PGUSER || 'lotus',
-  database: process.env.PGDATABASE || 'real_estate',
-  host: process.env.PGHOST || '/var/run/postgresql',
-  port: process.env.PGPORT || 5432,
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
-  process.exit(-1);
 });
 
 export default pool;
